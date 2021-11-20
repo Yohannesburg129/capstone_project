efc2edaefad49914dd6bd3cb02959c3c40b7d6b9
Johann Manukulasuriya <jmanukulasuriya@yahoo.com>
2021-11-20 13:19:31 -0500

    added capstone_code.py

---
+++ capstone_code.py
@@ -0,0 +1,18 @@
+# Import Numpy, Pandas, Matplotlib, and Seaborn
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load in the airline dataset
+df = pd.read_csv('capstone_airline_reviews 1.csv')
+
+# Check the dataset
+df.head()
+
+# Checking the shape of the dataset
+df.shape
+
+df.info()
+
